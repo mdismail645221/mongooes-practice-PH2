5d79f7eb96b3f7bea31c23d4b3e8a822428b4e1e
Ismail Hossain <you@example.com>
2023-05-12 23:58:55 +0600

    step 3 : model added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, {Application, Request, Response, NextFunction, urlencoded} from 'express';
 const app:Application = express()
 import cors from 'cors';
-import { Schema } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
 // middle ware
 //using cors
@@ -31,7 +31,7 @@ app.get('/', (req:Request, res:Response) => {
     middleName: string;
     lastName: string;
    }
-   dateOfBirth?: 'string';
+   dateOfBirth?: string;
    gender: 'male' | 'female';
    email?: string;
   contactNo: string;
@@ -43,8 +43,7 @@ app.get('/', (req:Request, res:Response) => {
   // step2: Schema user Interface 
   const userSchema = new Schema <IUser>({
     id: {type: String, required: true,unique: true},
-    role: {type: String,required: true,unique: true
-    },
+    role: {type: String,required: true,unique: true},
     password: {type: String,required: true},
     name: {
       firstName: {type: String,required:true},
@@ -62,10 +61,37 @@ app.get('/', (req:Request, res:Response) => {
   })
 
 
+  // step3 : Model 
+  const User = model<IUser>("User", userSchema);
+
+  //asyn fucntion
+  const createUserToDB =async () => {
+    const user = new User({
+      id: "9879",
+      role: 'student',
+      password: 'abcd',
+      name: {
+        firstName: 'abul1',
+        middleName: 'start1',
+        lastName: 'three1',
+      },
+      dateOfBirth: '24/07/19992',
+      gender: 'female',
+      email: 'back@gmail.com',
+      contactNo: '018721471003',
+      emergencyContactNo: '01872147103',
+      presentAddress: 'nai',
+      permanentAddress: 'naa'
+    })
+    await user.save()
+    console.log(user)
+  }
+
+  createUserToDB()
 
 
     res.send('Hello World!')
-  })
+})
 
 
 export default app;
